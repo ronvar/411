53b82fa3d3ac7ae05400677d7d49b4be284614a6
ronvar <31808077+ronvar@users.noreply.github.com>
2018-12-08 14:50:12 -0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-client_id = '75ee62cc223b4706a94a37c3233a558a'
-client_secret = 'f4ed656eaa944c1d874ac26c40e51710'
-redirect_uri = 'http://localhost:8888/callback'
-AzureAPIKey =  'b63d75bc949c49b6b1baa8fc020e9d97'
